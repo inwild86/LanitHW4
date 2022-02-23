e8f7dc951e8ee0290b18b9c9156f511013378df4
IW <lisa143.97@gmail.com>
2022-02-23 21:11:01 +0400

    fix#1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,8 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        String text = "The quick brown fox jumps over the lazy dog. ";
-        Pattern pattern = Pattern.compile(".+");
+        String text = "Jan 1987";
+        Pattern pattern = Pattern.compile("(.*(\\d{4}))");
         Matcher matcher = pattern.matcher(text);
         while (matcher.find()) {
             System.out.println(text.substring(matcher.start(), matcher.end()));
