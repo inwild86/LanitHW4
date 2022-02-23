7ddea9a717dce963febb64252bd9e805334e8355
IW <lisa143.97@gmail.com>
2022-02-23 20:42:39 +0400

    add homework4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
+        String text = "The quick brown fox jumps over the lazy dog. ";
+        Pattern pattern = Pattern.compile(".+");
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            System.out.println(text.substring(matcher.start(), matcher.end()));
+        }
+
 
     }
 }
+// 2  (.*)\d{3,9} (\d{4})  [A-Z,a-z]{3}( )(\d+)
